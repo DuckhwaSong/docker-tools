86247a75ed5b3d1533701d4bc874ee15333bc116
Duckhwa Song <kf80song@gmail.com>
2024-02-16 00:20:03 +0900

    apache-php-fpm 완성

--- apache-php-fpm/build.bat
+++ apache-php-fpm/build.bat
@@ -16,9 +16,9 @@ docker stop "%projectDir%" && docker rm "%projectDir%"
 docker build . -t img-%projectDir%
 
 :: 3. 도커 컨테이너 런
-::docker run -it -d -p %port%:80 -v "%CD%\html:/www/html" --name "%projectDir%" img-%projectDir%
+docker run -it -d -p %port%:80 -v "%CD%\html:/www/html" --name "%projectDir%" img-%projectDir%
 :: docker run -it -d -p %port%:80 --name "%projectDir%" img-%projectDir%
-docker run -it -d -v "%CD%\html:/www/html" --name "%projectDir%" img-%projectDir%
+::docker run -it -d -v "%CD%\html:/www/html" --name "%projectDir%" img-%projectDir%
 
 :: 4. 도커 컨테이너 아파치 + mysql 실행 
 :: docker exec -itd "%projectDir%" "/usr/sbin/httpd"
@@ -29,6 +29,7 @@ docker run -it -d -v "%CD%\html:/www/html" --name "%projectDir%" img-%projectDir
 :: docker cp apache-php-fpm:/etc/apache2/httpd.conf ./
 :: docker exec -it "%projectDir%" "/usr/bin/mysql -uroot -ppassword -e 'CREATE DATABASE firstmall;'"
 :: docker exec -it "%projectDir%" "/usr/bin/mysql -uroot -ppassword firstmall < /www/firstmall.sql"
+:: docker cp apache-php-fpm:/usr/lib/php83/modules/screwim.so ./
 
 :: 5. 도커 컨테이너 확인
 docker exec -it "%projectDir%" ps ax
